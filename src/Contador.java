f213d134dd5999fee622771f35a7bc2b29bac42e
Math012 <math111.0811@gmail.com>
2025-02-06 13:01:12 -0300

    Implementado a logica do contador

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,32 @@
+import exception.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro parametro inteiro: ");
+        int patametro1 = scanner.nextInt();
+
+        System.out.println("Informe o segundo parametro inteiro: ");
+        int patametro2 = scanner.nextInt();
+
+        try {
+            if (patametro1 > patametro2){
+                throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+            }
+            contar(patametro1,patametro2);
+        }catch (ParametrosInvalidosException e){
+            throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametro1, int parametro2){
+        int number = 1;
+        for (int i=parametro1; i<parametro2;i++){
+            System.out.println("Imprimindo o número " + number);
+            number++;
+        }
     }
 }
